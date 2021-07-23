86524606c5a79f5469ae494d4a8e837990fac315
Nazareh <nazarehturmina@gmail.com>
2021-07-23 15:44:54 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Route, Switch} from 'react-router-dom'
+
+const HatsPage = () =>
+    <div>
+        <h1> hellp my lord!</h1>
+    </div>
+;
 
 function App() {
-  return <HomePage/>;
+    return <div>
+        <Switch>
+            <Route path='/hats' component={HatsPage}/>
+            <Route path='/' component={HomePage}/>
+        </Switch>
+    </div>;
 }
 
 export default App;
