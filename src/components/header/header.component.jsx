c79f7648518530414c7c0def4652e8d8f8c80944
nazareh <nazarehturmina@gmail.com>
2021-10-13 16:24:58 +0800

    118. connect() and mapStateToProps

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,22 +1,41 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import {ReactComponent as Logo} from '../../assets/crown.svg';
-import {auth} from '../../firebase/firebase.utils';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import './header.styles.scss';
+import { auth } from "../../firebase/firebase.utils";
 
-const Header = ({currentUser}) => (
-    <div className='header'>
-        <Link className='logo-container' to='/'>
-            <Logo className='logo'/>
-        </Link>
-        <div className='options'>
-            <Link to='/shop' className='option'>SHOP NOW</Link>
-            <Link to='/' className='option'>CONTACT</Link>
-            {currentUser ? <div className='option' onClick={() => auth.signOut()}> SIGN OUT </div> :
-                < Link to='/signIn' className='option'> SIGN IN</Link>}
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+import "./header.styles.scss";
+
+const Header = ({ currentUser }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link to="/shop" className="option">
+        SHOP NOW
+      </Link>
+      <Link to="/" className="option">
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          {" "}
+          SIGN OUT{" "}
         </div>
+      ) : (
+        <Link to="/signIn" className="option">
+          {" "}
+          SIGN IN
+        </Link>
+      )}
     </div>
+  </div>
 );
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser,
+});
 
-export default Header;
+export default connect(mapStateToProps)(Header);
