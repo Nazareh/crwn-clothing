1008466845580e482cdab908cefbb11979d0922c
nazareh <nazarehturmina@gmail.com>
2021-10-21 21:06:30 +0800

    124. Add To Cart Styling

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./cart-dropdown.styles.scss";
-import CustomButton from '../custom-button/custom-button.component';
+import CustomButton from "../custom-button/custom-button.component";
 
 const CartDropdown = () => (
-    <div className='cart-dropdown'>
-        <div className='cart-items'>
-            <CustomButton> GO TO CHECKOUT</CustomButton>
-        </div>
-    </div>
-)
+  <div className="cart-dropdown">
+    <div className="cart-items" />
+    <CustomButton> GO TO CHECKOUT</CustomButton>
+  </div>
+);
 
 export default CartDropdown;
