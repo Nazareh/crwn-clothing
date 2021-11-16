5a231fae52eddfd2ba0c1be93a9cd98320566ec5
nazareh <nazarehturmina@gmail.com>
2021-11-16 22:29:23 +0800

    135. User Selectors

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
+import { createStructuredSelector } from "reselect";
+import { selectCurrentUser } from "./redux/user/user.selectors";
 
 class App extends Component {
   unsubscribeFromAuth = null;
@@ -50,8 +52,8 @@ class App extends Component {
   }
 }
 
-const matStateToProps = rootReducer => ({
-  currentUser: rootReducer.user.currentUser
+const matStateToProps = createStructuredSelector ({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = (dispatch) => ({
