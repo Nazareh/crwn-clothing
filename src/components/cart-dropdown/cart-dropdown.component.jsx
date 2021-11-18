e0ef142746240deb6797671ef36b51a6a289d64e
nazareh <nazarehturmina@gmail.com>
2021-11-18 20:06:41 +0800

    139. Dispatch Action Shorthand

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { connect } from "react-redux";
 
@@ -8,26 +7,33 @@ import CartItem from "../cart-item/cart-item.component";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
 import { createStructuredSelector } from "reselect";
 import { withRouter } from "react-router";
+import { toogleCartHidden } from "../../redux/cart/cart.actions";
 
-
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-      {
-      cartItems.length ?
-      cartItems.map(cartItem => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))
-      :
-      <span className='empty-message'> Your cart is empty</span>
-      }
+      {cartItems.length ? (
+        cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <span className="empty-message"> Your cart is empty</span>
+      )}
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}> GO TO CHECKOUT</CustomButton>
+    <CustomButton
+      onClick={() => {
+        history.push("/checkout");
+        dispatch(toogleCartHidden());
+      }}
+    >
+      {" "}
+      GO TO CHECKOUT
+    </CustomButton>
   </div>
 );
 
-const mapStateToProps = createStructuredSelector ({
-  cartItems: selectCartItems
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
 });
 
 export default withRouter(connect(mapStateToProps)(CartDropdown));
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -7,27 +7,16 @@ import {
 } from "../../redux/cart/cart.selectors";
 import { connect } from "react-redux";
 
+const headers = ["Product", "Description", "Quantity", "Price", "Remove"];
+
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
     <div className="checkout-header">
-      <div className="header-block">
-        <span>Product</span>
-      </div>
-      <div className="header-block">
-        <span>Description</span>
-      </div>
-      <div className="header-block">
-        <span>Quantity</span>
-      </div>
-      <div className="header-block">
-        <span></span>
-      </div>
-      <div className="header-block">
-        <span>Price</span>
-      </div>
-      <div className="header-block">
-        <span>Remove</span>
-      </div>
+      {headers.map((header) => (
+        <div className="header-block">
+          <span>{header}</span>
+        </div>
+      ))}
     </div>
     {cartItems.map((item) => item.name)}
     <div className="total">
